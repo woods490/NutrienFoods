2cf51ece6d4c978774e202da718de8e48e761848
nazlafirqina <firqirahmi@gmail.com>
2021-04-15 16:00:29 +0700

    ini index dan gambar paling baru

--- index.php
+++ index.php
@@ -1,11 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <link rel="stylesheet" href="./css/index.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nutrien Foods </title>
 </head>
+
 <body>
+    <ul class="header">
+        <li id="logo">NutrienFoods</li>
+        <li class="li-nav" >Home</li>
+        <li class="li-nav">Menu</li>
+        <li class="li-nav">About</li>
+        <li class="li-nav">Contact</li>
+        <li class="signUp">Sign Up</li>
+    </ul>
+    <div class="first">
+        <div class="kiri-atas">
+            <span class="title">A tasty and Healthy Food</span>
+            <p class="subtitle">We are here to provide a healthy and tasty food for you,
+                so you can eat well without worrying the calories and your weight.
+                The food here is being formulated by certified nutrionist
+                and having a laboratory test.</p>
+            <button id="buy">BUY NOW</button>
+        </div>
+        <div class="kanan-atas">
+            <img src="./img/—Pngtree—healthy food_3776802.png" alt="">
+        </div>
+    </div>
+    <div class="tengah">
+        <span class="title-2">Who Are We?</span>
+        <div class="container-tengah">
+            <div class="tengah-kiri">
+                <img src="./img/chef.png" alt="">
+            </div>
+            <div class="tengah-kanan">
+                <span class="small-title">NutrienFoods</span>
+                <p class="subtitle-2">NutrienFoods is a company who wants
+                    to provide a healthy meal for our customer. we realize
+                    that every customers has their own body goals and needs.
+                    So,we decided to make a 3 types of food depends on user’s body mass index . </p>
+            </div>
+        </div>
+    </div>
+    <span class="title-2">Types Of Dishes</span>
+    <div class="bawah">
+        <div class="card">
+            <img src="./img/—Pngtree—healthy food_3776802.png" alt="" class="imgcard">
+            <div class="small-title2">Loss Weight</div>
+            <p class="caption">This food is made for the one who wants to
+                loss some weight. </p>
+        </div>
+        <div class="card">
+            <img src="./img/hotpot.png" alt="" class="imgcard">
+            <div class="small-title2">Stabil Weight</div>
+            <p class="caption">This food is made for the one
+                who wants to stabilize their weight </p>
+        </div>
+        <div class="card">
+            <img src="./img/chickenRice.png" alt="" class="imgcard">
+            <div class="small-title2">Gain Weight</div>
+            <p class="caption">This food is made for the one who
+                wants to gain some weight.</p>
+        </div>
+    </div>
+    <footer>
+        <ul class="footer">
+            <li class="li-footer">Email : nutrienfoods@nf.com</li>
+            <li class="li-footer">Phone : 081234567890</li>
+            <li class="li-footer">Instagram : @nutrienfoods.id</li>
+        </ul>
+    </footer>
 </body>
+
 </html>
